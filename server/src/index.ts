b28723b655b5fb1497b3cf8bce2d7645b050f349
José Manuel Mármol Ávila <josemanuel.marmol@alu.ceacfp.es>
2026-04-20 10:05:33 +0200

    Cambiado index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import recetasRouter from './routes/recetas.routes'
 import favoritasRouter from './routes/favoritas.routes'
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = Number(process.env.PORT) || 3000
 
 app.use(cors({
   origin: [
@@ -17,6 +17,6 @@ app.use(express.json())
 app.use('/api/v1/recetas', recetasRouter)
 app.use('/api/v1/favoritas', favoritasRouter)
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
